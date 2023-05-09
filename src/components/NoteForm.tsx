e8d5675e1ee55af92a18de5aaded612f3228b079
Sreesanth46 <sreesanth46@gmail.com>
2023-05-09 13:54:30 +0530

    fix: defaultValue

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -9,16 +9,19 @@ type NoteFormProps = {
     onSubmit: (data: NoteData) => void;
     onAddTag: (tag: Tag) => void;
     availableTags: Tag[];
-};
+} & Partial<NoteData>;
 
 export const NoteForm = ({
     onSubmit,
     onAddTag,
     availableTags,
+    title = "",
+    markdown = "",
+    tags = [],
 }: NoteFormProps) => {
     const titleRef = useRef<HTMLInputElement>(null);
     const markdownRef = useRef<HTMLTextAreaElement>(null);
-    const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+    const [selectedTags, setSelectedTags] = useState<Tag[]>(tags);
     const navigate = useNavigate();
 
     const handleSubmit = (e: FormEvent) => {
@@ -40,7 +43,11 @@ export const NoteForm = ({
                     <Col>
                         <Form.Group controlId="title">
                             <Form.Label>Title</Form.Label>
-                            <Form.Control ref={titleRef} required />
+                            <Form.Control
+                                ref={titleRef}
+                                required
+                                defaultValue={title}
+                            />
                         </Form.Group>
                     </Col>
                     <Col>
@@ -83,6 +90,7 @@ export const NoteForm = ({
                         required
                         as="textarea"
                         rows={15}
+                        defaultValue={markdown}
                     />
                 </Form.Group>
                 <Stack
